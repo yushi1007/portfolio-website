f0a134566e3e7691115c560eedb6577101e6b62f
Yu Shi <yushiys95@gmail.com>
2021-05-21 16:51:06 -0400

    working on the home page

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import Typical from 'react-typical'
 
 function Home () {
     return(
         <div className="homepage">
-            <Fade delay={200} bottom cascade>
-                <div className="myname">
-                    <h1>YU SHI</h1>
-                </div>
-            </Fade>
-            <Fade delay={400} bottom >
-                <div className="mytitle">
-                    <h2>SOFTWARE ENGINEER</h2>
-                </div>
-            </Fade>
+            <div className="introduction">
+                <Fade delay={200} bottom cascade>
+                    <div className="myname">
+                        <h1>Hello world 👋, I'm Yu Shi</h1>
+                    </div>
+                </Fade>
+                <Fade delay={400} bottom >
+                    <div className="mytitle">
+                        <p>I'm a{' '}
+                            <Typical 
+                                loop={Infinity}
+                                wrapper="b"
+                                steps={[
+                                    "Full Stack Web Developer 💻",
+                                    1000,
+                                    "Photograher 📸",
+                                    1000,
+                                    "Designer 💅",
+                                    1000,
+                                    "Ready To Work With You 💼",
+                                    1000
+                                ]}
+                            />
+                        </p>    
+                    </div>
+                </Fade>
+                <Fade></Fade>
+            </div>
         </div>
     )
 }
