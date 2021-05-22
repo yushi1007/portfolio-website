afa44291b49f14f1ab01f940b2f1070567673cab
Yu Shi <yushiys95@gmail.com>
2021-05-21 23:46:10 -0400

    working on the about page

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import Resume from '../resume/resume.pdf'
 import Fade from 'react-reveal/Fade'
 import Slide from 'react-reveal/Slide'
 
 const About = () => {
     return(
     <div id="about" class="section section-summary js--section-summary">
-        <div className="row">
-        <Slide left duration={800}><h1>About Me</h1></Slide>
+        <div className="row about">
+        <Slide left duration={800}>
+            <h1>ABOUT ME</h1>
+        </Slide>
         <Fade bottom duration={1000} delay={150}>
             <p className="summary">
-                <strong>Hi there! my name is Yu Shi, a Software Engineer with experience building full stack applications with JavaScript, React, and Ruby on Rails.</strong>
-                <br/>Possess strong skills in designing, developing and testing multiple web-based applications. Data driven and creative in finding solutions to complex problems. Love to develop innovative applications that enable creativity.
-            </p>
+                <strong>Hi there! my name is Yu Shi, a Software Engineer with experience building full stack applications with JavaScript, React, and Ruby on Rails.</strong> Possess strong skills in designing, developing and testing multiple web-based applications. Data driven and creative in finding solutions to complex problems. Love to develop innovative applications that enable creativity.
+            </p><br/>
         </Fade>
         <Fade bottom duration={1000} delay={300}>
             <p className="summary">
@@ -27,6 +29,13 @@ const About = () => {
                     <li><a className="medium" href="https://yushi95.medium.com/"><i className="fab fa-medium-m"></i></a></li>
                     <li><a className="github" href="https://github.com/yushi1007"><i className="fab fa-github"></i></a></li>
                 </ul>
+            </div>
+        </Fade>
+        <Fade bottom duration={1000} delay={300} cascade>
+            <div className="resumelink">
+                <div>
+                  <a href={Resume} target="_blank" rel="noreferrer">View Resume</a>
+                </div>
             </div>
         </Fade>
     </div>
