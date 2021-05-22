c78ee19e90451f11cbb2c348706d93aa43399ed6
Yu Shi <yushiys95@gmail.com>
2021-05-22 12:35:49 -0400

    finished social media icons

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -24,10 +24,10 @@ const About = () => {
         <Fade bottom duration={1000} delay={600} cascade>
             <div className="icons">
                 <ul>
-                    <li><a className="linkedin" href="https://www.linkedin.com/in/yushi95"><i className="fab fa-linkedin"></i></a></li>
-                    <li><a className="instagram" href="https://www.instagram.com/yushi.95"><i className="fab fa-instagram"></i></a></li>
-                    <li><a className="medium" href="https://yushi95.medium.com/"><i className="fab fa-medium-m"></i></a></li>
-                    <li><a className="github" href="https://github.com/yushi1007"><i className="fab fa-github"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/yushi95"><i className="fab fa-linkedin"></i></a></li>
+                    <li><a href="https://www.instagram.com/yushi.95"><i className="fab fa-instagram"></i></a></li>
+                    <li><a href="https://yushi95.medium.com/"><i className="fab fa-medium-m"></i></a></li>
+                    <li><a href="https://github.com/yushi1007"><i className="fab fa-github"></i></a></li>
                 </ul>
             </div>
         </Fade>
