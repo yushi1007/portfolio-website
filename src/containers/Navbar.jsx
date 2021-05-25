7d63d26495237748e13ff0d45d01cf0ea4e3085f
Yu Shi <yushiys95@gmail.com>
2021-05-25 16:36:35 -0400

    working on navbar:

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -7,15 +7,16 @@ const Navbar = () => {
     return(
         <div id="navbar" className="navbar">
             <img src={Logo} alt="logo"/>
+            <h3>YU SHI</h3>
             <div className="nav">
-                <ul>
+                <ul className="navlink">
                     <li><Link to="home" activeClass="active" spy={true} smooth={true} duration={700}>Home</Link></li>
                     <li><Link to="about" activeClass="active" spy={true} smooth={true} duration={700}>About</Link></li>
                     <li><Link to="skills" activeClass="active" spy={true} smooth={true} duration={700}>Skills</Link></li>
                     <li><Link to="projects" activeClass="active" spy={true} smooth={true} duration={700}>Projects</Link></li>
                     <li><Link to="blogs" activeClass="active" spy={true} smooth={true} duration={700}>Blogs</Link></li>
-                    <li><Link to="contact" activeClass="active" spy={true} smooth={true} duration={700}>Contact</Link></li>
-                    <li><a href={Resume} target="_blank" rel="noreferrer">View Resume</a></li>
+                    <li><a href={Resume} target="_blank" rel="noreferrer">Resume</a></li>
+                    <li><Link to="contact" activeClass="active" spy={true} smooth={true} duration={700}><button className="contact-btn">Contact</button></Link></li>
                 </ul>
             </div>
         </div>
