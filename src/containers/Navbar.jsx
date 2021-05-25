fc36c7fa657af78351c9355b2d4421d11b434822
Yu Shi <yushiys95@gmail.com>
2021-05-25 18:29:48 -0400

    navbar done

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -7,10 +7,8 @@ const Navbar = () => {
     return(
         <div id="navbar" className="navbar-box">
             <div className="navbar">
-                <img src={Logo} alt="logo"/>
-                <h3>Yu Shi</h3>
-                <div className="nav">
                     <ul className="navlink">
+                        <li><Link to="about" activeClass="active" spy={true} smooth={true} duration={700}><img src={Logo} alt="logo"/></Link></li>
                         <li><Link to="home" activeClass="active" spy={true} smooth={true} duration={700}>Home</Link></li>
                         <li><Link to="about" activeClass="active" spy={true} smooth={true} duration={700}>About</Link></li>
                         <li><Link to="skills" activeClass="active" spy={true} smooth={true} duration={700}>Skills</Link></li>
@@ -19,7 +17,6 @@ const Navbar = () => {
                         <li><a href={Resume} target="_blank" rel="noreferrer">Resume</a></li>
                         <li><Link to="contact" activeClass="active" spy={true} smooth={true} duration={700}><button className="navbar-btn">Contact</button></Link></li>
                     </ul>
-                </div>
             </div>
         </div>
     )
