85c54fa918e8c978e42a1811891731491c9dedce
Yu Shi <yushiys95@gmail.com>
2021-05-25 20:07:29 -0400

    working on footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './styles/App.css';
 import About from './containers/About';
 import Home from './containers/Home';
 import Navbar from './containers/Navbar';
 import Skills from './containers/Skills';
 import Projects from './containers/Projects';
 import Blogs from './containers/Blogs';
-import './styles/App.css';
 import Contact from './containers/Contact';
 import Footer from './containers/Footer';
 
