38d5ae999117659d019d8e74b0710a5532b86d6a
Yu Shi <yushiys95@gmail.com>
2021-05-25 20:13:05 -0400

    add bouce effect on the navbar

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from "react-scroll"
 import Logo from '../images/mylogo.png';
 import Resume from '../resume/resume.pdf'
+import Bounce from 'react-reveal/Bounce';
 
 const Navbar = () => {
     return(
         <div id="navbar" className="navbar-box">
             <div className="navbar">
+            <Bounce right cascade duration={800}>
                     <ul className="navlink">
                         <li><Link to="about" activeClass="active" spy={true} smooth={true} duration={700}><img src={Logo} alt="logo"/></Link></li>
                         <li><Link to="home" activeClass="active" spy={true} smooth={true} duration={700}>Home</Link></li>
@@ -17,6 +19,7 @@ const Navbar = () => {
                         <li><a href={Resume} target="_blank" rel="noreferrer">Resume</a></li>
                         <li><Link to="contact" activeClass="active" spy={true} smooth={true} duration={700}><button className="navbar-btn">Contact</button></Link></li>
                     </ul>
+            </Bounce>
             </div>
         </div>
     )
